b5e54a8a00a347fa71c74257a8b865ec7d1c411c
Администратор <@DESKTOP-E5S22RC>
2025-12-19 21:08:50 +0300

    Добавьте файлы проекта.

---
+++ 9 praktika/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_praktika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            string name = "анна";
+            string NameFormat2 = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+            string city = "москва";
+            string CITY = city.ToUpper();
+            Console.WriteLine(NameFormat2);
+            Console.WriteLine(CITY);
+            Console.WriteLine($"Привет, {NameFormat2} из {CITY}!");
+
+            ////Задание 2
+            string text = "C# это современный язык программирования.";
+            Console.WriteLine("Общее количество символов: ");
+            int len = text.Length;
+            string[] words = text.Split(' ');
+            Console.WriteLine(text.Length);
+            Console.WriteLine("Количество слов: ");
+            int wordCount = words.Length;
+            Console.WriteLine(wordCount);
+            Console.WriteLine("Индекс: ");
+            int position = text.IndexOf("язык");
+            Console.WriteLine(position);
+            bool start = text.StartsWith("C#");
+            Console.WriteLine("Текст начинается с с#: " + start);
+
+
+            //Задание 3
+            string phone = "+79161234567";
+            string Country = phone.Substring(0, 2);
+            string Operator = phone.Substring(2, 3);
+            string number123 = phone.Substring(5, 3);
+            string number45 = phone.Substring(8, 2);
+            string number67 = phone.Substring(10, 2);
+            string phoneFormat2 = $"{Country} ({Operator}) {number123}-{number45}-{number67} ";
+            Console.WriteLine(phoneFormat2);
+
+
+            //Задание 4
+            string email = "user.example@gmail.com";
+            Console.WriteLine(email);
+            if (email.Contains("@"))
+            {
+                int index = email.IndexOf("@");
+                string user = email.Substring(0, index);
+                string gmail = email.Substring(index + 1);
+                string star = new string('*', user.Length);
+                string email2 = $"{star}@{gmail}";
+                Console.WriteLine($"Скрытый email: {email2}");
+            }
+
+
+            //Задание 5
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine().ToLower();
+            int left = 0;
+            int right = word.Length - 1;
+            bool Palindrom = true;
+            while (left < right)
+            {
+                if (word[left] != word[right])
+                {
+                    Palindrom = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            if (Palindrom)
+            {
+                Console.WriteLine($"Слово {word} полидром");
+            }
+            else
+            {
+                Console.WriteLine($"Слово {word} не полидром");
+            }
+
+
+            //Задание 6
+            StringBuilder sb = new StringBuilder("Числа: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                sb.Append(i);
+                if (i < 10)
+                {
+                    sb.Append(',');
+                }
+            }
+            sb.Append('.');
+            Console.WriteLine(sb.ToString());
+
+
+            // Задание 7
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    sb.Append($"{i} x {j} = {i * j}");
+                    sb.AppendLine();
+                }
+                if (i < 5)
+                {
+                    sb.AppendLine();
+                }
+            }
+            Console.WriteLine(sb.ToString());
+
+            // Задание 8
+            string fruits = "яблоко,банан ,aпельсин,киви, груша";
+            string[] fruitArray = fruits.Split(',');
+            for (int i = 0; i < fruitArray.Length; i++)
+            {
+                Console.WriteLine(fruitArray[i]);
+            }
+
+            for (int i = 0; i < fruitArray.Length; i++)
+            {
+                fruitArray[i] = fruitArray[i].Trim();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < fruitArray.Length; i++)
+            {
+                Console.WriteLine(fruitArray[i]);
+            }
+            Console.WriteLine();
+            Array.Sort(fruitArray);
+            for (int i = 0; i < fruitArray.Length; i++)
+            {
+                Console.WriteLine(fruitArray[i]);
+            }
+            Console.WriteLine();
+            string result = string.Join("; ", fruitArray);
+            Console.WriteLine($"Ожидаемый результат: " + result);
+
+            //Задание 9
+            Console.WriteLine("Введите слово на английском строчными буквами");
+            string shifr = Console.ReadLine();
+            int sdvig = 3;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in shifr)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    char cezar = (char)(((c - 'a' + sdvig) % 26) + 'a');
+                    sb.Append(cezar);
+                }
+            }
+            Console.WriteLine($"Зашифровано: {sb}");
+
+            //Задание 10
+            Random random = new Random();
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                char lowerLetter = (char)('a' + random.Next(0, 26));
+                password.Append(lowerLetter);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                char digit = (char)('0' + random.Next(0, 10));
+                password.Append(digit);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                char upLetter = (char)('A' + random.Next(0, 26));
+                password.Append(upLetter);
+            }
+            Console.WriteLine("Пример возможного пароля: " + password);
+
+
+        }
+    }
+}
---
+++ 9 praktika/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("9 praktika")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9 praktika")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a4a85a58-8558-4805-ade5-829f7e8b9123")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
